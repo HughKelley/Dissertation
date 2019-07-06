d031bae88e6e8047e83e5e42af01140751a3878a
Hugh <hghklly@gmail.com>
2019-07-06 14:42:23 +0000

    more work on custom filter

--- Analysis/custom_filter.py
+++ Analysis/custom_filter.py
@@ -6,7 +6,10 @@ Created on Fri Jul  5 15:10:38 2019
 """
 
 
-import osmnx as ox
+#import osmnx as ox
+
+
+
 ox.config(use_cache=True, log_console=True)
 
 
@@ -14,6 +17,29 @@ ox.config(use_cache=True, log_console=True)
 
 filter_string = ('["area"!~"yes"]["highway"!~"footway|steps|corridor|motor|proposed|construction|abandoned|platform|raceway"]'
                     '["bicycle"!~"no"]["service"!~"private"]{}').format(settings.default_access)
+
+
+
+#
+#'["area"!~"yes"]
+#["highway"!~"footway|steps|corridor|motor|proposed|construction|abandoned|platform|raceway"]'
+#                    '["bicycle"!~"no"]["service"!~"private"]{}'
+# !~ is the negation operator. 
+# this query works by selecting all stuff that is not an area, is a highway that is not one of those things, 
+# is not labelled no bicycle and is not private. 
+
+#default_access = '["access"!~"private"]'
+
+
+#
+# query_template = '[out:json][timeout:{timeout}]{maxsize};({infrastructure}{filters}(poly:"{polygon}");>;);out;'
+# query_str = query_template.format(polygon=polygon_coord_str, infrastructure=infrastructure, filters=osm_filter, timeout=timeout, maxsize=maxsize)
+
+
+#def osm_net_download(polygon=None, north=None, south=None, east=None, west=None,
+#                     network_type='all_private', timeout=180, memory=None,
+#                     max_query_area_size=50*1000*50*1000, infrastructure='way["highway"]',
+#                     custom_filter=None):
 
 
 filter_string = 'blank'

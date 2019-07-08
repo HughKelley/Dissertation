6a8637749189563b106ca94aa3d0b75e59515a28
Hugh <hghklly@gmail.com>
2019-07-08 16:15:59 +0000

    pull o/d pairs without connection from quant data

---
+++ Analysis/quant_data.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  8 14:55:53 2019
+
+@author: Hugh
+"""
+
+#psuedo code
+#
+#build dataframe of all O/D combos that do not have a value for travel time
+#
+
+# to take a look at the first line of the file
+# import pandas as pd
+# df = pd.read_csv(quant_file, nrows = 2)
+
+
+import time
+start = time.time()
+
+quant_file = 'Data/quant/dis_bustuberail_london_500.csv'
+lines_read = 0
+lines_wrote = 0
+zeros = 0
+
+f = open(quant_file, "r")
+out = open("Data/quant/quant_zeros.csv", "w")
+
+out.write(f.readline())
+for line in f:
+    data = line.split(",")
+    if float(data[3]) != 0:
+#        lines_read = lines_read + 1
+        zeros = zeros + 1
+        continue
+    
+#    skip lines with same o and d
+    if str(data[0]) == str(data[1]):
+        lines_read = lines_read +1
+        continue
+    
+    out.write(line)
+    lines_wrote = lines_wrote + 1
+    lines_read = lines_read + 1
+#    print("read: ", lines_read, "wrote: ", lines_wrote)
+#    printing is slow, but need to check correctness
+    
+    
+print("runtime in seconds: ", time.time() - start)
+    
+print("read: ", lines_read, "wrote: ", lines_wrote)
+print("zeros: ", zeros)
+
+
+f.close()
+out.close()      
+
+
+#build dataframe of LSOA's bordering Origin LSOA 
+#
+#add travel time for each of those LSOA's to DEstination LSOA
+#
+#Build Dataframe of lsoa's bording destination LSOA
+#
+#add travel time from Origin LSOA to destination border LSOA's 
+#
+#take min walking plus transit travel time and use as overall time in main matrix
+
+
+
+
+
+
+
+
+
+
+
+#import pandas as pd
+#import csv
+#import geopandas
+
+#quant = 'Data/dis_bustuberail_london500/dis_bustuberail_london500.csv'
+#quant = "C:/Users/Hugh\Documents/UCL_CASA_1819/Dissertation/Analysis/Data/quant/dis_bustuberail_london_500.csv"
+#subset = open("Data/)
+
+#f = open(quant, "r")
+
+
+
+
+#data = pd.read_csv(quant, nrows = 1)
+
+#
+#with open(quant) as file:
+#    reader = csv.reader(file, delimiter = ',')
+#    df = pd.DataFrame(list(reader))
+
+
+# easily hits memory limits trying to load into df. 
+    
+#    process:
+#       read line by line, taking only lines with zeros. 
+#       any line with a zero, save to a dataframe
+#       
+
+
+
+#quant = 'Data/dis_bustuberail_london_500.csv'
+
+#quant_data = pd.read_csv(quant)
+
+#
+#with open(quant) as f: 
+#    content = f.readlines()

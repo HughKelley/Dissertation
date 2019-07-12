6c5f4cb091476cbe2109c20d93169c7bdbd776fe
Hugh <hghklly@gmail.com>
2019-07-12 11:55:21 +0000

    clean and build file for adding shapefiles to dissDB

---
+++ Analysis/shp_to_postgres.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 10 11:02:56 2019
+
+@author: Hugh
+"""
+
+# modules 
+import pandas as pd
+import geopandas as gpd
+import psycopg2
+from geoalchemy2 import Geometry, WKTElement
+import sqlalchemy
+
+# get data file
+file = "Data/statistical-gis-boundaries-london/ESRI\LSOA_2011_London_gen_MHW.shp"
+LSOA_shp = gpd.read_file(file)
+#print(LSOA_shp)
+
+column_names = list(LSOA_shp.columns.values)
+column_types = list(LSOA_shp.dtypes)
+
+# where dtype is 'O', python is indicating that the column is not a base type, it's a python Object
+
+##############################################################################
+# with sqlalchemy engine
+###################################################################
+
+engine = sqlalchemy.create_engine('postgresql://postgres:tuesday789@host:5432/dissertation')
+
+
+
+
+
+
+##############################################################################
+# with psycopg2
+###################################################################
+
+# start connection to DB
+conn = psycopg2.connect(database="Dissertation", user="postgres", password="tuesday789")
+cur = conn.cursor()
+
+#cur.execute("CREATE TABLE lsoa (id serial PRIMARY KEY, LSOA11CD varchar(32), LSOA11NM varchar(32), geom geometry);")
+# looks in pgAdmin like this was fine and postgis is installed correctly because geometry dat type was accepted? 
+
+# put the data from the shapefile as a geodataframe into the table
+
+
+# about to be so easy with SQLalpchemy GEO extension
+# https://gis.stackexchange.com/questions/239198/adding-geopandas-dataframe-to-postgis-table/239231
+
+# does this need to iterate through the rows of the GDF?
+cur.execute("INSERT INTO lsoa VALUES " from GDF)
+
+
+# put upload code here
+
+
+
+# cur.execute("CREATE TABLE test2 (id serial PRIMARY KEY, num integer, data varchar);")
+#
+# cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)",(100, "abc'def"))
+#
+# cur.execute("SELECT * FROM test;")
+#
+# cur.fetchone()
+
+
+conn.commit()
+
+cur.close()
+
+conn.close()

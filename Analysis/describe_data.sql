8f318c526a1e8dd7bb7dec074b7e8922a5aa3477
Hugh <hghklly@gmail.com>
2019-07-22 15:51:54 +0000

    add index to table

--- Analysis/describe_data.sql
+++ Analysis/describe_data.sql
@@ -12,4 +12,21 @@
 
 -- SELECT DISTINCT "value" from osm_way_tags WHERE "key" = 'maxspeed';
 
-SELECT COUNT(*) from osm_way_tags WHERE "key" = 'maxspeed';
+-- SELECT COUNT(*) from osm_way_tags WHERE "key" = 'maxspeed';
+
+-- SELECT * from lsoa_table LIMIT 5;
+
+CREATE INDEX ind on lsoa_table ["LSOA11CD"];
+
+
+
+
+-- DROP INDEX public.ind;
+
+CREATE INDEX ind
+    ON public.lsoa_table USING btree
+    ("LSOA11CD" COLLATE pg_catalog."default" varchar_ops)
+    TABLESPACE pg_default;
+
+COMMENT ON INDEX public.ind
+    IS 'just a test';

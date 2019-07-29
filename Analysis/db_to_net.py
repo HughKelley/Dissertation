c52ab320517d4d497be2ad965a3160072eed7bee
Hugh <hghklly@gmail.com>
2019-07-29 12:57:19 +0000

    calc node extended node stats direct from download. Mem error

--- Analysis/db_to_net.py
+++ Analysis/db_to_net.py
@@ -17,8 +17,8 @@ engine = sqlalchemy.create_engine('postgresql://postgres:tuesday789@localhost:54
 
 # pull tables into gdf's
 
-node_query = 'SELECT * FROM london_drive_nodes'
-edge_query = 'SELECT * FROM london_drive_edges'
+node_query = 'SELECT * FROM london_drive_projected_nodes'
+edge_query = 'SELECT * FROM london_drive_Projected_edges'
 
 # convert to geodataframe
 # http://geopandas.org/reference.html?highlight=wkb#geopandas.GeoDataFrame.from_postgis
@@ -26,9 +26,13 @@ edge_query = 'SELECT * FROM london_drive_edges'
 nodes = gpd.GeoDataFrame.from_postgis(sql = node_query, con = engine, geom_col = 'geom', crs= 27700)
 edges = gpd.GeoDataFrame.from_postgis(sql = edge_query, con = engine, geom_col = 'geom', crs= 27700)
 
+# gdfs_to_graph needs a gdf_name key set
 nodes.gdf_name = 'nodes'
-
 edges.gdf_name = 'edges'
+
+# ox.basic_stats needs the geometry column to be named geometry not geom
+nodes.rename(columns={'geom':'geometry'}, inplace=True)
+edges.rename(columns={'geom':'geometry'}, inplace=True)
 
 # ox.graph_to_gdfs() returns gdf with structure:
 #            highway       osmid         x          y                       geometry
@@ -41,6 +45,10 @@ edges.gdf_name = 'edges'
 # turn gdf's into network
 net = ox.gdfs_to_graph(gdf_nodes = nodes, gdf_edges = edges)
 
+graph_area_m = nodes.unary_union.convex_hull.area
+# 377,743,665.1522919 for drive
 
+net_stats = ox.basic_stats(net, area = graph_area_m, clean_intersects=True, circuity_dist='euclidean')
+# gives multiple KeyErrors and a ValueError
 
 # 
---
+++ Analysis/net_stats.py
@@ -0,0 +1,161 @@
+import osmnx as ox
+import pandas as pd  
+
+import geopandas as gpd
+from geoalchemy2 import Geometry, WKTElement
+
+import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# get the data types
+from sqlalchemy import Column, Integer, String, Float, BigInteger
+from geoalchemy2 import Geometry
+from geoalchemy2.shape import to_shape 
+
+# set up sql connection
+engine = sqlalchemy.create_engine('postgresql://postgres:tuesday789@localhost:5432/Dissertation')
+
+Base = declarative_base(engine)
+metadata = sqlalchemy.MetaData()
+
+class geo_crs_lbound(Base):
+
+	__table__ = sqlalchemy.Table('geo_crs_lbound', metadata, autoload=True, autoload_with=engine)
+
+# Function to generate WKB hex
+def wkb_hexer(line):
+    return line.wkb_hex
+
+
+# convert geometry to usable type
+def geom_column_to_wkb(gdf):
+
+	gdf['geom'] = gdf['geometry'].apply(wkb_hexer)
+	gdf.drop('geometry', 1, inplace=True)
+
+	return gdf
+
+def change_geom(net_type, engine, crs):
+	""""""
+
+	node_table_name = net_type + '_nodes'
+	edge_table_name = net_type + '_edges'
+
+	tables = [ node_table_name, edge_table_name]
+
+	for table in tables:
+
+		with engine.connect() as conn, conn.begin():
+			sql = 'ALTER TABLE ' + table + ' ALTER COLUMN geom TYPE Geometry(Polygon, ' + str(crs) + ') USING ST_SetSRID(geom::Geometry, ' + str(crs) + ')'
+			conn.execute(sql)
+
+	return 0
+
+
+def get_poly(session, table):
+
+	boundary_data = session.query(table).all()
+
+	boundary_geom = boundary_data[0].geom 
+
+	shapely_geom = to_shape(boundary_geom)
+
+	# buffer to get valid outer ring polygon instead of invalid multipolygon. 
+
+	valid_poly = shapely_geom.buffer(0)
+
+	return valid_poly
+
+
+
+def loadSession():
+	""""""
+	Session = sessionmaker(bind=engine)
+	session = Session()
+	return session
+
+
+if __name__ == "__main__":
+
+
+	# get boundary data
+
+	session = loadSession()
+
+	polygon = get_poly(session, geo_crs_lbound)
+
+
+
+	# define list of network types
+	# file_list = ['all', 'bike', 'walk']
+	file_list = ['drive']
+
+	item = 'drive'
+
+	# for item in file_list:
+
+		# print(item)
+
+		# get the network from osm
+
+	name = 'london_' + item + '_projected'
+	geo_graph = ox.graph_from_polygon(polygon, network_type = item, name = name)
+	graph = ox.project_graph(geo_graph)
+
+	# project graph using osmnx function instead of doing it in postgis
+
+	# convert the network to 2 gdf's
+
+	nodes, edges = ox.graph_to_gdfs(graph, nodes = True, edges = True, node_geometry = True, fill_edge_geometry = True)
+
+	print('retrieved')
+
+	# graph_area
+	graph_area_m = nodes.unary_union.convex_hull.area
+	print('graph area: ', graph_area_m)
+
+	# calc net stats from projected network 
+	net_stats = ox.basic_stats(graph, area = graph_area_m, clean_intersects=True, circuity_dist='euclidean')
+
+
+
+
+		# convert the geometry of each gdf to WKB
+
+		# nodes = geom_column_to_wkb(nodes)
+		# edges = geom_column_to_wkb(edges)
+
+		# # put the gdf into the DB
+
+		# nodes.to_sql((name + '_nodes') , engine, if_exists='fail', index=False)
+		# edges.to_sql((name + '_edges') , engine, if_exists='fail', index=False)
+
+		# print('to sql successfully')
+
+		# alter the geometry of the column
+
+		# crs = str(4326)
+
+		# with engine.connect() as conn, conn.begin():
+		# 	sql = 'ALTER TABLE ' + name + '_nodes' + ' ALTER COLUMN geom TYPE Geometry(Point, ' + crs + ') USING ST_SetSRID(geom::Geometry, ' + crs + ')'
+		# 	conn.execute(sql)
+
+		# with engine.connect() as conn, conn.begin():
+		# 	sql = 'ALTER TABLE ' + name + '_edges' + ' ALTER COLUMN geom TYPE Geometry(LineString, ' + crs + ') USING ST_SetSRID(geom::Geometry, ' + crs + ')'
+		# 	conn.execute(sql)
+
+
+
+# CRS notes: 
+	# UTM is universal transverse mercator
+	# a projected crs
+	# WSG84 is World Geodetic System from 1984
+	# Geographic coordinate system in lat and lon
+
+
+# projection module offers: 
+	# project_gdf
+	# project_geometry
+	# project_graph
+	# https://osmnx.readthedocs.io/en/stable/osmnx.html?highlight=coordinate%20reference%20system#osmnx.projection.project_geometry
